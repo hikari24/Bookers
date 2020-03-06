6e652e9170ec0dafab6e3df254ba47bb5fc0de76
松山ひかり <a.hikari24@gmai.com>
2020-03-07 00:01:50 +0900

    3/6 update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,13 @@ class BooksController < ApplicationController
   def index
   		@books = Book.all
   		@book = Book.new
+
   	
   end
   
 
   def show
+      @book = Book.find(params[:id])
 
   end
 
@@ -33,7 +35,14 @@ class BooksController < ApplicationController
   def update
   		book = Book.find(params[:id])
   		book.update(book_params)
-  		redirect_to book_path(book)
+  		redirect_to book_path
+
+  end
+
+  def destroy
+  		book = Book.find(params[:id]) #データを１件取得
+  		book.destroy
+  		redirect_to books_path
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#top'
+  root to: 'homes#top'
 
   resources :books
 
